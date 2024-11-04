c5cc5a3b104e337040299163674cbef2bd0a9411
eeT <truongle.02102004@gmail.com>
2024-11-04 11:48:57 +0700

    test api tu local3000/testAPI sang local8080

--- src/App.js
+++ src/App.js
@@ -3,29 +3,47 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import {routes} from './routes'
 import HeaderComponent from './components/HeaderComponent/HeaderComponent'  
 import DefaultComponent from './components/DefaultComponent/DefaultComponent'
+import { Color } from 'antd/es/color-picker'
 
 
-function App() {
-  
-  return (
-    <div>
-      <Router>
-        <Routes>
-          {routes.map((route) => {
-            const Page = route.page
-            const Layout =  route.isShowHeader ? DefaultComponent : Fragment
-            return (
-              <Route key={route.path} path={route.path} element={
-              <Layout>
-                <Page />
-              </Layout>
-              }/>
-            )           
-          })}
-        </Routes>
-      </Router>
-    </div>
-  ) 
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={apiResponse:""}
+  }
+
+  callAPI(){
+    fetch("http://localhost:3000/testAPI")
+    .then(res => res.text())
+    .then(res => this.setState({apiResponse: res}));
+  }
+
+  componentWillMount(){
+    this.callAPI()
+  }
+  render(){
+    return (
+      <div className='App'>
+        
+        <Router>
+          <Routes>
+            {routes.map((route) => {
+              const Page = route.page
+              const Layout =  route.isShowHeader ? DefaultComponent : Fragment
+              return (
+                <Route key={route.path} path={route.path} element={
+                <Layout>
+                  <Page />
+                </Layout>
+                }/>
+              )           
+            })}
+          </Routes>
+        </Router>
+        <p>{this.state.apiResponse}</p>
+      </div>
+    ); 
+  }
 }
 
 export default App
